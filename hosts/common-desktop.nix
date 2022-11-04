c1acfdad1ce5430d36c8a1240672461e517e0f1a
surfaceflinger <nat@nekopon.pl>
2022-11-04 23:34:17 +0100

    fix printing and update lock file

--- hosts/common-desktop.nix
+++ hosts/common-desktop.nix
@@ -9,12 +9,19 @@
   networking.networkmanager.enable = true;
   services.avahi = {
     enable = true;
-    nssmdns = true;
     publish = {
       enable = true;
       userServices = true;
     };
   };
+
+  # fix for resolving printers through mdns/avahi
+  services.avahi.nssmdns = false;
+  system.nssModules = with pkgs.lib; optional (!config.services.avahi.nssmdns) pkgs.nssmdns;
+  system.nssDatabases.hosts = with pkgs.lib; optionals (!config.services.avahi.nssmdns) (mkMerge [
+    (mkOrder 900 [ "mdns4_minimal [NOTFOUND=return]" ]) # must be before resolve
+    (mkOrder 1501 [ "mdns4" ]) # 1501 to ensure it's after dns
+  ]);
 
   # Desktop environment
   services.xserver = {
