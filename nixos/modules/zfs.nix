52b28a83fa88774159df265d56187384a5b3e0ea
surfaceflinger <nat@nekopon.pl>
2022-12-14 12:56:26 +0100

    Remove zfs workaround because I'm a genius

--- nixos/modules/zfs.nix
+++ nixos/modules/zfs.nix
@@ -1,6 +1,5 @@
 {config, ...}: {
   boot = {
-    initrd.postDeviceCommands = "zpool import -a -d ${config.boot.zfs.devNodes}";
     supportedFilesystems = ["zfs"];
     kernelParams = ["init_on_alloc=0" "init_on_free=0" "zfs.zfs_abd_scatter_enabled=0" "zfs.zfs_compressed_arc_enabled=0"];
   };
