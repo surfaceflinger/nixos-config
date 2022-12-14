248eacce9c31421d3a19c46be17ca33df9182eb3
surfaceflinger <nat@nekopon.pl>
2022-12-14 01:06:14 +0100

    EPIC zfs workaround

--- nixos/modules/zfs.nix
+++ nixos/modules/zfs.nix
@@ -1,5 +1,6 @@
-{...}: {
+{config, ...}: {
   boot = {
+    initrd.postDeviceCommands = "zpool import -a -d ${config.boot.zfs.devNodes}";
     supportedFilesystems = ["zfs"];
     kernelParams = ["init_on_alloc=0" "init_on_free=0" "zfs.zfs_abd_scatter_enabled=0" "zfs.zfs_compressed_arc_enabled=0"];
   };
