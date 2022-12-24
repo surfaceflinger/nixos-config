44957a6f7fc6a4603eb3886e8f6d8539167ef826
surfaceflinger <nat@nekopon.pl>
2022-12-24 23:37:42 +0100

    switch nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "nat's nixos configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     impermanence.url = "github:nix-community/impermanence";
     xkomhotshot = {
